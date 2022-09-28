2d212c12a263b3dcd7667c8f62b1a1968d916498
marcusee <marcus.ee@novade.net>
2022-09-28 21:14:33 +0800

    Delete Everything

--- component---src-pages-page-2-js-1a82707af7090a4b00fc.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{230:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(152),c=a(226),r=a(225);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-1a82707af7090a4b00fc.js.map
--- styles-4677dd51ed13b101c196.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{275:function(n,w,o){}}]);
-//# sourceMappingURL=styles-4677dd51ed13b101c196.js.map
